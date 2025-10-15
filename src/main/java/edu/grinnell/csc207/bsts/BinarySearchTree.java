3840bcb6f7382da420c19331cc947fd2011e7b50
Malek <malekmat@mauchly.cs.grinnell.edu>
2025-10-15 17:22:16 -0500

    FInished up till delete

--- src/main/java/edu/grinnell/csc207/bsts/BinarySearchTree.java
+++ src/main/java/edu/grinnell/csc207/bsts/BinarySearchTree.java
@@ -1,6 +1,8 @@
 package edu.grinnell.csc207.bsts;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A binary tree that satisifies the binary search tree invariant.
@@ -64,41 +66,100 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
         return sizeH(root);
     }
 
-    /**
-     * @param value the value to add to the tree
-     */
-    public void insert(T value) {
-        throw new UnsupportedOperationException();
+    /** @return the updated tree after inserting h into the given tree */
+    private Node<T> insertH(T v, Node<T> cur) {
+        if (cur == null) {
+            return new Node<>(v);
+        } else {
+            if (v.compareTo(cur.value) < 0) {
+                cur.left = insertH(v, cur.left);
+            } else {
+                cur.right = insertH(v, cur.right);
+            }
+            return cur;
+            }
+        }
+        public void insert(T v) { root = insertH(v, root);
+    }
+
+
+    public boolean containsHelper(Node<T> n, T value) {
+        if(n == null) {
+            return false;
+        }
+        int comp = n.value.compareTo(value);
+        if(comp == 0) {
+            return true;
+        } else if(comp > 0) {
+            return containsHelper(n.left, value);
+        }
+        return containsHelper(n.right, value);
+        
     }
 
     ///// Part 1: Contains
-   
     /**
+       The contains method will check for the given value by first checking if the tree is empty,
+     * then checking whether the value is less than or greater then to the value in root, then either descending
+     * down the left or right branch. If the current node is equal to null, then the function returns false. If the current
+     * node has the desired value, the method returns true.
+     * @param value the value to search for
+     * 
      * @param v the value to find
      * @return true iff this tree contains <code>v</code>
      */
     public boolean contains(T v) {
-        throw new UnsupportedOperationException();
+        return containsHelper(root, v);
     }
 
     ///// Part 2: Ordered Traversals
 
-    /**
+   
+    public void toStringHelper(StringBuffer l, Node<T> n) {
+        if(n != null) {
+            toStringHelper(l, n.left);
+            l.append(n.value);
+            l.append(", ");
+            toStringHelper(l, n.right);
+        }
+    }
+
+     /**
      * @return the (linearized) string representation of this BST
      */
-    @Override
     public String toString() {
-        throw new UnsupportedOperationException();
+        StringBuffer str = new StringBuffer("[");
+        if(root != null){
+            toStringHelper(str, root);
+            str.delete(str.length()-2, str.length());
+        }
+        str.append("]");
+        return str.toString();
+    }
+
+
+    public void toListHelper(List<T> l, Node<T> n) {
+        if(n != null) {
+            toListHelper(l, n.left);
+            l.add(n.value);
+            toListHelper(l, n.right);
+        }
     }
 
     /**
      * @return a list contains the elements of this BST in-order.
      */
     public List<T> toList() {
-        throw new UnsupportedOperationException();
+        List<T> l = new ArrayList<T>();
+        toListHelper(l, root);
+        return l;
     }
 
     ///// Part 3: BST Sorting
+
+    public static <T extends Comparable<? super T>> void sortHelper(List<T> lst, Node<T> tree) {
+
+    }
 
     /**
      * @param <T> the carrier type of the lists
@@ -107,17 +168,58 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
      * @implSpec <code>sort</code> runs in ___ time if the tree remains balanced. 
      */
     public static <T extends Comparable<? super T>> List<T> sort(List<T> lst) {
-        throw new UnsupportedOperationException();
+        BinarySearchTree<T> tree = new BinarySearchTree<T>();
+        for(int i = 0; i < lst.size(); i++){
+            tree.insert(lst.get(i));
+        }
+        return tree.toList();
     }
 
     ///// Part 4: Deletion
   
     /*
      * The three cases of deletion are:
-     * 1. (TODO: fill me in!)
-     * 2. (TODO: fill me in!)
-     * 3. (TOOD: fill me in!)
+     * 1. if curr_node.left is null, curr_node is set to node.right
+     * 2. if curr_node.right is null, curr_node is set to node.left
+     * 3. if neither is null, sets curr_node to be 
      */
+
+     public void deleteH(Node<T> curr_node){
+        Node<T> temp1 = curr_node;
+        Node<T> temp = curr_node.right;
+        temp1 = temp1.left;
+        while(temp1.right != null){
+            temp1 = temp1.right;
+        }
+        temp1.right = temp;
+
+        curr_node = curr_node.left;
+     }
+
+    public void deleteRecur(Node<T> n, T value) throws IllegalArgumentException{
+        if(n == null) {
+            return;
+        }
+        int comp = n.value.compareTo(value);
+        if(comp == 0) {
+            if(n.right == null && n.left == null){
+                n = null;
+            } else if(n.right == null) {
+                n = n.left;
+            } else if(n.left == null){
+                n = n.right;
+            } else {
+                deleteH(n);
+
+            }
+            return;
+        } else if(comp > 0) {
+            deleteRecur(n.left, value);
+        }
+            deleteRecur(n.right, value);
+        
+    }
+    
 
     /**
      * Modifies the tree by deleting the first occurrence of <code>value</code> found
@@ -126,6 +228,6 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
      * @param value the value to delete
      */
     public void delete(T value) {
-        throw new UnsupportedOperationException();
+        deleteRecur(root, value);
     }
 }
--- src/test/java/edu/grinnell/csc207/trees/BSTTests.java
+++ src/test/java/edu/grinnell/csc207/trees/BSTTests.java
@@ -15,6 +15,11 @@ public class BSTTests {
         BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
         // TODO: make a sample tree consisting of 3, 5, 2, 6, and 4
         //       through repeated calls to insert.
+        tree.insert(3);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(6);
+        tree.insert(4);
         return tree;
     }
 
@@ -39,23 +44,23 @@ public class BSTTests {
 
     @Test
     public void basicToStringTest() {
-        assertEquals("[0, 5, 6, 7, 11]", mkSampleTree().toString());
+        assertEquals("[2, 3, 4, 5, 6]", mkSampleTree().toString());
     }
 
     @Test
     public void basicToListTest() {
         // N.B., need to upcast the Integer[] array to avoid a ClassCastException that
         // arises from downcasting the result of toArray to Integer[].
-        assertArrayEquals((Object[]) new Integer[] {0, 5, 6, 7, 11},
+        assertArrayEquals((Object[]) new Integer[] {2, 3, 4, 5, 6},
                           mkSampleTree().toList().toArray());
     }
 
     @Test
     public void basicSortTest() {
         assertArrayEquals(
-            (Object[]) new Integer[] {0, 5, 6, 7, 11},
+            (Object[]) new Integer[] {2, 3, 4, 5, 6},
             BinarySearchTree.<Integer> sort(
-                java.util.Arrays.asList(0, 5, 6, 7, 11)).toArray());
+                java.util.Arrays.asList(2, 3, 4, 5, 6)).toArray());
     }
 
     @Test
